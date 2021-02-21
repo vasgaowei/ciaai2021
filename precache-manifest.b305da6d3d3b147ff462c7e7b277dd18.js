7f49a35d67addaf7f4003007a59024dd554b820f
vasgaowei <vasgaowei@gmail.com>
2021-02-21 22:29:44 +0800

    Updates

---
+++ precache-manifest.b305da6d3d3b147ff462c7e7b277dd18.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98bebd17dd249d1ea584ef0f56b92009",
+    "url": "/cicai2021/index.html"
+  },
+  {
+    "revision": "cc8a25842afb1d6cd166",
+    "url": "/cicai2021/static/css/main.a270b3a9.chunk.css"
+  },
+  {
+    "revision": "62ce0b969bf055117176",
+    "url": "/cicai2021/static/js/2.afa959f7.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/cicai2021/static/js/2.afa959f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc8a25842afb1d6cd166",
+    "url": "/cicai2021/static/js/main.4aa454b3.chunk.js"
+  },
+  {
+    "revision": "059c0d7227d34b325a35",
+    "url": "/cicai2021/static/js/runtime-main.fb9e16b7.js"
+  },
+  {
+    "revision": "3372362a56440e6e824cf24a6246bf0d",
+    "url": "/cicai2021/static/media/Toronto-skyline.3372362a.jpg"
+  },
+  {
+    "revision": "cbbe3b55d26f3bfc5c3ebe35299cdcf9",
+    "url": "/cicai2021/static/media/header-background.cbbe3b55.jpg"
+  },
+  {
+    "revision": "4332610edd61faf335fa05b752f2f523",
+    "url": "/cicai2021/static/media/ieee.4332610e.png"
+  },
+  {
+    "revision": "cbbe3b55d26f3bfc5c3ebe35299cdcf9",
+    "url": "/cicai2021/static/media/keynote_speaker_1.cbbe3b55.png"
+  },
+  {
+    "revision": "11d3bb4deaf36059c9f8c2929cfed81c",
+    "url": "/cicai2021/static/media/keynote_speaker_2.11d3bb4d.png"
+  },
+  {
+    "revision": "e1fb59a10fa3f6b62d4e3bb11c7f19ad",
+    "url": "/cicai2021/static/media/keynote_speaker_3.e1fb59a1.png"
+  },
+  {
+    "revision": "addc169317131e482d28053ba2b37e57",
+    "url": "/cicai2021/static/media/keynote_speaker_4.addc1693.png"
+  },
+  {
+    "revision": "4aa53f8ea67921a8cc42893ae66441d6",
+    "url": "/cicai2021/static/media/ubc.4aa53f8e.png"
+  },
+  {
+    "revision": "5d0cd1b087f4d70f6025c29b49fdb188",
+    "url": "/cicai2021/static/media/uot.5d0cd1b0.png"
+  }
+]);
