c83d7ebc6a85ec4dd2454c96ec8e1e2b150ab3c2
vasgaowei <vasgaowei@gmail.com>
2021-02-25 02:02:16 +0800

    Updates

---
+++ precache-manifest.6e664bf7d1480f18e9ea6c9437c73e03.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8327f70f96718dd2ddc5f650636d0346",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c2e50df65d193cb708e",
+    "url": "/static/css/main.8e67cd65.chunk.css"
+  },
+  {
+    "revision": "62ce0b969bf055117176",
+    "url": "/static/js/2.afa959f7.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/static/js/2.afa959f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c2e50df65d193cb708e",
+    "url": "/static/js/main.5ead44ca.chunk.js"
+  },
+  {
+    "revision": "4b8255d7d6b28a2c5cf5",
+    "url": "/static/js/runtime-main.f8958408.js"
+  },
+  {
+    "revision": "3372362a56440e6e824cf24a6246bf0d",
+    "url": "/static/media/Toronto-skyline.3372362a.jpg"
+  },
+  {
+    "revision": "cbbe3b55d26f3bfc5c3ebe35299cdcf9",
+    "url": "/static/media/header-background.cbbe3b55.jpg"
+  },
+  {
+    "revision": "4332610edd61faf335fa05b752f2f523",
+    "url": "/static/media/ieee.4332610e.png"
+  },
+  {
+    "revision": "cbbe3b55d26f3bfc5c3ebe35299cdcf9",
+    "url": "/static/media/keynote_speaker_1.cbbe3b55.png"
+  },
+  {
+    "revision": "11d3bb4deaf36059c9f8c2929cfed81c",
+    "url": "/static/media/keynote_speaker_2.11d3bb4d.png"
+  },
+  {
+    "revision": "e1fb59a10fa3f6b62d4e3bb11c7f19ad",
+    "url": "/static/media/keynote_speaker_3.e1fb59a1.png"
+  },
+  {
+    "revision": "addc169317131e482d28053ba2b37e57",
+    "url": "/static/media/keynote_speaker_4.addc1693.png"
+  },
+  {
+    "revision": "4aa53f8ea67921a8cc42893ae66441d6",
+    "url": "/static/media/ubc.4aa53f8e.png"
+  },
+  {
+    "revision": "5d0cd1b087f4d70f6025c29b49fdb188",
+    "url": "/static/media/uot.5d0cd1b0.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cicai2021/precache-manifest.b305da6d3d3b147ff462c7e7b277dd18.js"
+  "/precache-manifest.6e664bf7d1480f18e9ea6c9437c73e03.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cicai2021/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
